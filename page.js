8562a3bd46ad212f55f8cc852544d6bebc028ae2
SaeedAzarafrooz <SaeedAzarafrooz@users.noreply.github.com>
2023-07-17 18:58:30 +0000

    deploy: 208f0c3e746053c1fa18dd28942b50d939570252

--- page.js
+++ page.js
@@ -5,7 +5,7 @@ export default function Home() {
     <main className={styles.main}>
       <div>
         <h1 className={styles.H1}>Main section</h1>
-        <span>56465462fghfgh45646</span>
+        <span>56465462fg</span>
       </div>
     </main>
   );
