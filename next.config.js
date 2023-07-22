68b9d1aedb0589242cb9a6f090d6be252570339e
saeed azarafrooz <saeed_azarafrooz@yahoo.com>
2023-07-22 10:11:08 +0330

    feat: add recursion for menu2

--- next.config.js
+++ next.config.js
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: "export",
-    // images: { unoptimized: true }
-    images: {
-        loader: 'custom',
-        loaderFile: './app/image.js',
-      },
+    images: { unoptimized: true }
+    // images: {
+    //     loader: 'custom',
+    //     loaderFile: './app/image.js',
+    //   },
 }
 
 module.exports = nextConfig
