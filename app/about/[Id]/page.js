0486f11b4464bb6d1427ce29a004403df0ef05a5
saeed azarafrooz <saeed_azarafrooz@yahoo.com>
2023-07-25 22:08:25 +0330

    test dynamic routing3

--- app/about/[Id]/page.js
+++ app/about/[Id]/page.js
@@ -1,4 +1,34 @@
-'use client';
+import getData from '../../requests/GetData'
+
+
+export const getStaticPaths=async () => {
+    const res=await fetch('http://localhost:8000/menus');
+    const data = await res.json();
+    const paths=data.map(menus=>{
+        return{
+            params:{id: menus.id.toString()}
+        }
+    })
+    return {
+        paths,
+        fallback: false
+    }
+}
+
+// export const getStaticProps=async (context)=>{
+//     const id=context.params.id;
+//     const res=await fetch('http://localhost:8000/menus/'+id);
+//     const data = await res.json();
+//     return{
+//         props: {menus:data}
+//     }
+// }
+// export default async function UserPosts({params}) {
+//     const userFirstPost = await getUserPosts(params['userId'])
+//     const postData = userFirstPost[0]
+
+// }
+
 export default function AboutDetails({ params }) {
-    return <h1>About Page id: {params.Id}</h1>;
+    return <h1>About Page id:  - {params.id}</h1>;
   }
