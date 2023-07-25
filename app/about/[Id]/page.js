16484cfac39d995f5e846589e1aab8f094ba8b7f
saeed azarafrooz <saeed_azarafrooz@yahoo.com>
2023-07-25 20:22:29 +0330

    test dynamic routing2

--- app/about/[Id]/page.js
+++ app/about/[Id]/page.js
@@ -1,3 +1,4 @@
+'use client';
 export default function AboutDetails({ params }) {
     return <h1>About Page id: {params.Id}</h1>;
   }
--- app/page.js
+++ app/page.js
@@ -2,6 +2,7 @@ import styles from "./css/HomePage.module.css";
 import Image from 'next/image'
 import profilePic from './images/vercel.svg'
 import profilePic2 from './images/next.svg'
+import Link from "next/link";
 
 
 export default function Home() {
@@ -9,6 +10,7 @@ export default function Home() {
     <main className={styles.main}>
       <div>
         <h1 className={styles.H1}>Main section</h1>
+        <Link href="/about/10">about/10</Link>
       </div>
     </main>
   );
